af9a65bec993219960267a98d5a0e6318dafa4d7
Unknown <bram-19-98@Hotmail.nl>
2020-04-01 11:41:55 +0200

    added classes

---
+++ Pokemon.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Pokemon {
+
+public $name;
+public $max_health;
+public $health;
+public $energytype;
+public $energytypes;
+public $attack;
+public $weakness;
+
+function __construct($name, $max_health, $health, $energytype, $energytypes, $attack, $weakness){
+$this->name = $name
+$this->max_health = $max_health
+$this->health = $health
+$this->$energytype = $energytype
+$this->$energytypes = $energytypes
+$this->attack = $attack
+$this->weakness = $weakness
+
+}
+
+
+
+}
+
+
+
+
+
+ ?>
